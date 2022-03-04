e6aec673e4bd5cca03b248fd83505fe194dc95f9
Leo Oladimu <leooladimu9@gmail.com>
2022-03-04 08:38:17 -0500

    4

--- api/server.js
+++ api/server.js
@@ -1,20 +1,18 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 
-const configureMiddleware = require('./middleware.js');
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
-const recipesRouter = require('../recipes/recipes-router.js')
-
+const recipesRouter = require('../recipes/recipes-router.js');
 
 const server = express();
 
-configureMiddleware(server);
-
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
 server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
 server.use('/api/recipes', recipesRouter);
-
-
 
 server.get('/', (req, res) => {
   res.send('TAKE TWO!!');
